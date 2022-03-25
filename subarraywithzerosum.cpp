be761c8f1d3e3618249a77bc3abb4866f2e2055e
Roli Gautam <72242893+RoliGautam@users.noreply.github.com>
2022-03-25 19:12:52 +0530

    Update subarraywithzerosum.cpp

--- subarraywithzerosum.cpp
+++ subarraywithzerosum.cpp
@@ -21,9 +21,7 @@ bool subArrOSum(int arr[], int n){
     for(int i = 0; i<n; i++){
         presum += arr[i];
         if(s.find(presum) != s.end()){
-            
-
-            return true;
+          return true;
         }else if(presum == 0) return true;
         else s.insert(arr[i]);
     }
