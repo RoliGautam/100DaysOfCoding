4c73e9f65b328be8b2756d45ef5449ac9e34a188
Roli Gautam <72242893+RoliGautam@users.noreply.github.com>
2022-04-24 23:37:07 +0530

    Create sortColors.DNFALGO.cpp

---
+++ Day-30/sortColors.DNFALGO.cpp
@@ -0,0 +1,26 @@
+//using dutch national flag algo
+ void sortColors(vector<int>& arr) {
+        int n = arr.size();
+        int l = 0;
+        int mid = 0;
+        int h = n-1;
+        while(mid<=h){
+            switch(arr[mid]){
+                case 0:
+                    swap(arr[l], arr[mid]);
+                    l++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr[h], arr[mid]);
+                    h--;
+                    break;
+            }
+        }
+     
+    }
+
+//time complexity: 0(n)
